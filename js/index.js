762bc1a0a61c27eb096207e045e6127602418eed
Fabricio <fabriciot89@gmail.com>
2023-04-13 22:51:06 -0300

    dia1

--- js/index.js
+++ js/index.js
@@ -1,24 +1,70 @@
-function search() {
-    let filter = document.getElementsById('find').value.toUpperCase();
+// function search() {
+//     let filter = document.getElementsById('find').value.toUpperCase();
 
-    let item = document.querySelectorAll ('.product');
+//     let item = document.querySelectorAll ('.product');
 
-    let l = document.getElementsByTagName ('h3');
+//     let l = document.getElementsByTagName ('h3');
 
-    for ( var i = 0;i<=l.length; i++) {
-        let a=item [i] .getElementsByTagName (h3)[0];
+//     for ( var i = 0;i<=l.length; i++) {
+//         let a=item [i] .getElementsByTagName (h3)[0];
 
-        let value=a.innerHTML || a.innerHTML || a.textContent;
+//         let value=a.innerHTML || a.innerHTML || a.textContent;
 
-        if (value.toUpperCase().indexOf(filter) > -1) {
-            item[i].style.display= "";
-            
-        }
-        else 
-        {
+//         if (value.toUpperCase().indexOf(filter) > -1) {
+//             item[i].style.display= "";
 
-            item[i].style.display="none";            
-        }
+//         }
+//         else 
+//         {
+
+//             item[i].style.display="none";            
+//         }
+//     }
+
+// }
+
+
+
+//Buscador de Conentenido//
+
+//Declaramos variables
+
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("cover-ctn-search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
+
+// Funcion para ejecutar el buscador
+
+document.getElementById("icon-search").addEventListener("click")
+
+//Creando filtrado de busqueda
+
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+
+function buscador_interno() {
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+
+    for (i = 0; i < li.lenght; i++) {
+
+        a = li[i].getElementByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if (textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+
+        } else {
+        li[i].style.display = "none";
+
     }
-    
-}
+
+  }
+
+    }
